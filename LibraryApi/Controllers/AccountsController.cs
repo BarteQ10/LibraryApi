749b26bfcee3f3f68c6e651f014eb3b14db373a9
Bartek <barteq2345@gmail.com>
2023-05-17 23:19:59 +0200

    Work on user Rights

---
+++ LibraryApi/Controllers/AccountsController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryApi.Data;
+using LibraryApi.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Controllers
+{
+    public class AccountsController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class AccountController : ControllerBase
+        {
+            private readonly ApplicationDbContext _context;
+
+            public AccountController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            
+        }
+    }
+}
